88cf05b2a41b9f05d125a58cf7e9809ddfb61e1f
Brandon Roberts <robertsbt@gmail.com>
2023-11-01 20:37:42 -0500

    chore: add test value

--- client/src/server/routes/v1/hello.ts
+++ client/src/server/routes/v1/hello.ts
@@ -1,6 +1,9 @@
 import { defineEventHandler } from 'h3';
 
-export default defineEventHandler((event) => ({
-  message: 'Hello World',
-  database: event.context.cloudflare.env.DATABASE_URL,
-}));
+export default defineEventHandler((event) => {
+  console.log(event.context);
+  return {
+    message: 'Hello World',
+    database: event.context.cloudflare?.env.DATABASE_URL || 'not set',
+  };
+});
