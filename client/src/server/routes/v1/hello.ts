9352d0e904f8a9fe5794dc117e5c3f88fd172fea
Brandon Roberts <robertsbt@gmail.com>
2023-11-01 21:00:12 -0500

    chore: use nitro env variable

--- client/src/server/routes/v1/hello.ts
+++ client/src/server/routes/v1/hello.ts
@@ -3,7 +3,7 @@ import { defineEventHandler } from 'h3';
 export default defineEventHandler((event) => {
   console.log('cloudflare', event.context.cloudflare);
   console.log('pe', process.env);
-  const dbUrl = useRuntimeConfig(event).DATABASE_URL;
+  const dbUrl = process.env.NITRO_DATABASE_URL;
 
   return {
     message: 'Hello World',
