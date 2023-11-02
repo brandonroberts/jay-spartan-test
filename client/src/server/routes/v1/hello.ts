0d0374c830d2ebc485e6a1e1fbd6e3a12c1e2ea4
Brandon Roberts <robertsbt@gmail.com>
2023-11-01 20:46:12 -0500

    chore: more logging

--- client/src/server/routes/v1/hello.ts
+++ client/src/server/routes/v1/hello.ts
@@ -1,7 +1,9 @@
 import { defineEventHandler } from 'h3';
 
 export default defineEventHandler((event) => {
-  console.log(event.context);
+  console.log('cloudflare', event.context.cloudflare);
+  console.log('pe', process.env);
+
   return {
     message: 'Hello World',
     database: event.context.cloudflare?.env.DATABASE_URL || 'not set',
